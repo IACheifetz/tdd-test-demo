04ad269730108145e8a1f2375e3b239ed6ddad6c
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-10 14:27:49 -0800

    added extra expect statements

--- test/functions.test.js
+++ test/functions.test.js
@@ -40,8 +40,13 @@ test('exclamations', (expect) => {
 
     const actual2 = addExclamationPoints('bunny rabbit');
 
+    const expected3 = 'bunny' + '!!!';
+
+    const actual3 = addExclamationPoints('bunny');
+
     expect.equal(actual, expected);
     expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'exclamations');
 });
 
 test('multiply7', (expect) => {
@@ -49,7 +54,17 @@ test('multiply7', (expect) => {
 
     const actual = multiplyBySeven(4);
 
+    const expected2 = 35;
+
+    const actual2 = multiplyBySeven(5);
+
+    const expected3 = 42;
+
+    const actual3 = multiplyBySeven(6);
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'multiply7');
 });
 
 test('multiply12/5', (expect) => {
@@ -57,7 +72,17 @@ test('multiply12/5', (expect) => {
 
     const actual = multiplyBy12ThenHalve(4);
 
+    const expected2 = 30;
+
+    const actual2 = multiplyBy12ThenHalve(5);
+
+    const expected3 = 36;
+
+    const actual3 = multiplyBy12ThenHalve(6);
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'multiply12/5');
 });
 
 test('divideThenMultiply', (expect) => {
@@ -65,7 +90,17 @@ test('divideThenMultiply', (expect) => {
 
     const actual = divideThenMultiply(8, 4, 5);
 
+    const expected2 = 14;
+
+    const actual2 = divideThenMultiply(10, 5, 7);
+
+    const expected3 = 45;
+
+    const actual3 = divideThenMultiply(15, 3, 9);
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'divideThenMultiply');
 });
 
 test('returnAsArray', (expect) => {
@@ -73,7 +108,17 @@ test('returnAsArray', (expect) => {
 
     const actual = returnAsAnArray(8, 4, 5);
 
+    const expected2 = [9, 3, 7];
+
+    const actual2 = returnAsAnArray(9, 3, 7);
+
+    const expected3 = [1, 2, 6];
+
+    const actual3 = returnAsAnArray(1, 2, 6);
+
     expect.deepEqual(actual, expected);
+    expect.deepEqual(actual2, expected2);
+    expect.deepEqual(actual3, expected3, 'returnAsArray');
 });
 
 test('returnAsString', (expect) => {
@@ -81,7 +126,17 @@ test('returnAsString', (expect) => {
 
     const actual = returnAsAString('8', '4', '5');
 
+    const expected2 = '169';
+
+    const actual2 = returnAsAString('1', '6', '9');
+    
+    const expected3 = '237';
+
+    const actual3 = returnAsAString('2', '3', '7');
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'returnAsString');
 });
 
 test('luckyGreeting', (expect) => {
@@ -89,15 +144,35 @@ test('luckyGreeting', (expect) => {
 
     const actual = makeLuckyGreeting(8, 4);
 
+    const expected2 = 'Hello! Your lucky number for the day is 8.';
+
+    const actual2 = makeLuckyGreeting(5, 3);
+
+    const expected3 = 'Hello! Your lucky number for the day is 12.';
+
+    const actual3 = makeLuckyGreeting(9, 3);
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'luckyGreeting');
 });
 
 test('SecondItem', (expect) => {
     const expected = 'apple';
 
     const actual = getSecondItem(['kiwi', 'apple', 'orange', 'plum']);
+    
+    const expected2 = 'guava';
+
+    const actual2 = getSecondItem(['banana', 'guava', 'mango', 'lychee']);
+    
+    const expected3 = 'strawberry';
+
+    const actual3 = getSecondItem(['grape', 'strawberry', 'blackberry', 'cherry']);
 
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'SecondItem');
 });
 
 test('lastItem', (expect) => {
@@ -105,7 +180,17 @@ test('lastItem', (expect) => {
 
     const actual = getLastItem(['kiwi', 'apple', 'orange', 'plum']);
 
+    const expected2 = 'lychee';
+
+    const actual2 = getLastItem(['banana', 'guava', 'mango', 'lychee']);
+    
+    const expected3 = 'cherry';
+
+    const actual3 = getLastItem(['grape', 'strawberry', 'blackberry', 'cherry']);
+
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3, 'lastItem');
 });
 
 test('randoNumber', (expect) => {
